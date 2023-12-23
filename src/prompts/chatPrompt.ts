2de0a35771566c46154cd031d7dc581c445cde01
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-23 15:49:37 -0300

    refactor: add new prompt and new context field

--- src/prompts/chatPrompt.ts
+++ src/prompts/chatPrompt.ts
@@ -3,20 +3,27 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { ChatPromptTemplate } from "langchain/prompts";
 import handlerVectorDB from "../services/applicationService/handlerVectorDB";
 
-export const chatPrompt = async (question: string) => {
-  const context = await handlerVectorDB.search(question);
+export const chatPrompt = async (command: string) => {
+  const copywriteKnowledge = await handlerVectorDB.searchCopywriteKnowledge(
+    // eslint-disable-next-line prettier/prettier
+    "Boas práticas para escrever um bom conteúdo"
+  );
+  const context = await handlerVectorDB.search(command);
 
   if (context?.length === 0) {
     return "Desculpe, mas não tenho informações sobre esse assunto";
   }
 
-  const chat = new ChatOpenAI({ temperature: 0 });
+  const chat = new ChatOpenAI({
+    temperature: 0,
+    modelName: "gpt-3.5-turbo-1106",
+  });
   const chatPrompt = ChatPromptTemplate.fromMessages([
     [
       "system",
-      "Você é um assistente que ajuda pessoas na tomada de decisão com base nas informações de contexto {context}, caso você não tenha contexto sobre a resposta, apenas diga que não sabe, não dê respostas fora do seu context.",
+      "Como escritor fantasma especializado em criar conteúdos envolventes para redes sociais, utilizo as melhores práticas de copywriting, as quais estão disponíveis por meio do {copywriteKnowledge}. Meu objetivo é fornecer textos relevantes e persuasivos, alinhados com as estratégias eficazes de comunicação online. Para otimizar ainda mais a qualidade do conteúdo, levo em consideração as informações de contexto específicas fornecidas por {context}. Caso não tenha acesso a informações detalhadas sobre a resposta necessária, serei transparente ao afirmar que não possuo o contexto adequado, evitando respostas que estejam fora do escopo da situação.",
     ],
-    ["human", "{question}"],
+    ["human", "{command}"],
   ]);
 
   const chain = new LLMChain({
@@ -25,8 +32,9 @@ export const chatPrompt = async (question: string) => {
   });
 
   const message = await chain.call({
+    copywriteKnowledge: copywriteKnowledge?.map((ctx) => ctx.pageContent),
     context: context?.map((ctx) => ctx.pageContent),
-    question,
+    command,
   });
 
   return message;
