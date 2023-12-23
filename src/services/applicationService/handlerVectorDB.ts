d64f1292f781e8ceba75840410346f250b37a4ba
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-12-23 15:50:24 -0300

    refactor: implements new functions

--- src/services/applicationService/handlerVectorDB.ts
+++ src/services/applicationService/handlerVectorDB.ts
@@ -2,7 +2,11 @@
 import prisma from "../../lib/prisma";
 import { PrismaVectorStore } from "langchain/vectorstores/prisma";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { Prisma, Document as PrismaDocument } from "@prisma/client";
+import {
+  Prisma,
+  Document as PrismaDocument,
+  CopywriteKnowledge,
+} from "@prisma/client";
 import { Document } from "langchain/dist/document";
 
 export const save = async (embedDocs: Document<Record<string, any>>[]) => {
@@ -33,7 +37,37 @@ export const save = async (embedDocs: Document<Record<string, any>>[]) => {
   }
 };
 
-export const search = async (question: string) => {
+export const saveCopywriteKnowledge = async (
+  embedDocs: Document<Record<string, any>>[]
+) => {
+  try {
+    const vectorStore = PrismaVectorStore.withModel<CopywriteKnowledge>(
+      prisma
+    ).create(new OpenAIEmbeddings(), {
+      prisma: Prisma,
+      tableName: "CopywriteKnowledge",
+      vectorColumnName: "vector",
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    });
+
+    await vectorStore.addModels(
+      await prisma.$transaction(
+        embedDocs.map((content) =>
+          prisma.copywriteKnowledge.create({
+            data: { content: content.pageContent },
+          })
+        )
+      )
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const search = async (command: string) => {
   try {
     const vectorStore = new PrismaVectorStore(new OpenAIEmbeddings(), {
       db: prisma,
@@ -46,7 +80,40 @@ export const search = async (question: string) => {
       },
     });
 
-    const documents = await vectorStore.similaritySearch(question, 10);
+    const documents = await vectorStore.similaritySearch(command, 10);
+
+    const context = documents.filter((document) => {
+      if (document.metadata?._distance) {
+        return (
+          Number(
+            document.metadata._distance
+              .toString()
+              .match(/^-?\d+(\.\d{1,2})?/)?.[0] || 0
+          ) <= 0.24
+        );
+      }
+    });
+
+    return context;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const searchCopywriteKnowledge = async (command: string) => {
+  try {
+    const vectorStore = new PrismaVectorStore(new OpenAIEmbeddings(), {
+      db: prisma,
+      prisma: Prisma,
+      tableName: "CopywriteKnowledge",
+      vectorColumnName: "vector",
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    });
+
+    const documents = await vectorStore.similaritySearch(command, 10);
 
     const context = documents.filter((document) => {
       if (document.metadata?._distance) {
@@ -68,7 +135,9 @@ export const search = async (question: string) => {
 
 const pdfService = {
   save,
+  saveCopywriteKnowledge,
   search,
+  searchCopywriteKnowledge,
 };
 
 export default pdfService;
